c63bbb989c838599d9a23d2a3c33a5b7cc100a55
Timothy <erekaketimothy09@gmail.com>
2022-10-13 16:26:03 -0700

    printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry
  * Return: Always 0
  */
- init main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+init main(void)
+{	
+       	  puts("\"Programming is like building a multilingual puzzle");
+     	  return (0);
+}	
