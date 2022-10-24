a0e74a6fb9962589bda47e967f7f3afdf643d6f8
Timothy <erekaketimothy09@gmail.com>
2022-10-24 12:29:32 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,14 @@
 void print_rev(char *s)
 {
 	int i;
-
-	length = 0;
+	int length = 0;
 
 	for (length = 0; s[length] != '\0'; length++)
 	{
 
 	}
 
-	for (i = length - 1; i >= 0, i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
