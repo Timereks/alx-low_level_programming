17992c5ffc30230f9e1e1d22246f5d18f80c56a8
Timothy <erekaketimothy09@gmail.com>
2022-10-24 13:31:17 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @str: string to be printed
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
