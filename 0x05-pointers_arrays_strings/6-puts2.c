23a223e241bbde275ebdbb5bf6b199f1e7024b8d
Timothy <erekaketimothy09@gmail.com>
2022-10-25 12:38:14 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,12 @@ void puts2(char *str)
 {
 	int i;
 
-	while (str[i] != '\0')
+	for (; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
-		{
+		if ((i % 2) == 0)
 			_putchar(str[i]);
-		}
-		i++;
+		else
+			continue;
 	}
 	_putchar('\n');
 }
