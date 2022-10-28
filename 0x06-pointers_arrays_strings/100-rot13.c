d016b64f70216894581eb1b6c2a0498e17d83943
Timothy <erekaketimothy09@gmail.com>
2022-10-27 17:45:58 -0700

    task 2

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,9 +9,9 @@ char *rot13(char *s)
 {
 	int i = 0;
 
-	for (s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
+		for ((s[i] >= 'a' && s[i] <= 'z') ||
 				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') ||
