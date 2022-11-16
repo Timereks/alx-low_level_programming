2dc33ef8f9aa8aa3b29d03a1d1e3fae174e1431d
Timothy <erekaketimothy09@gmail.com>
2022-11-16 09:29:29 -0800

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef funtion_pointers_h
-#define function_pointers_h
+#ifndef _funtion_pointers_h
+#define _function_pointers_h
+#include<stdlib.h>
 void print_name(char *name, void (*f)(char *));
 
 
